e8f9cb764c714c18593191d498458d7a3aa4ddb2
Shawn Wu <xywu333@gmail.com>
2019-12-01 18:53:36 +1100

    add sampel files for import account information

---
+++ account_sample.js
@@ -0,0 +1,8 @@
+module.exports={
+    username:'username', // repalce this string with your yeeyi username 
+    password:'pa22w0rd', // repalce this string with your yeeyi password 
+    links: ['www.yeeyi.com/bbs/thread-xxxxxxxx-1-1.html',
+        'www.yeeyi.com/bbs/thread-xxxxxxxx-1-1.html'
+    ]
+        // Put yeeyi post html in "links" array, link format should be "www.yeeyi.com/bbs.thread-xxxxxxx-1-1.html"
+}
