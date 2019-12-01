b75aea1838ab82aa45583a17e2e524cef437dd99
Shawn Wu <xywu333@gmail.com>
2019-12-01 18:46:38 +1100

    enable pushing more than one post now

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-const puppeteer = require('puppeteer');
-const {username,password} = require('./account');
+const puppeteer = require("puppeteer");
+const { username, password, links } = require("./account");
 
 (async () => {
   const browser = await puppeteer.launch();
   const loginPage = await browser.newPage();
-  await loginPage.goto('http://www.yeeyi.com/forum/index.php?app=member&act=login');
-  console.log('==Input Username and Password==');
-  await loginPage.type('#telTxtLogin', username);
-  await loginPage.type('#passShow', password);
-  await loginPage.screenshot({path: 'beforeClick.png'});
-  console.log('==Click Button ==');
-  loginPage.on('dialog', async dialog => {
-    console.log(dialog.message());
-    console.log('==Go to elevate thread link ==');
-    const page = await browser.newPage()
-    await page.goto('http://www.yeeyi.com/bbs/forum.php?mod=post&action=refresh&fid=142&id=4595329');
-    await page.screenshot({path: 'afterElevated.png'});
-    await browser.close();
+  await loginPage.goto(
+    "http://www.yeeyi.com/forum/index.php?app=member&act=login"
+  );
+  console.log("==Input Username and Password==");
+  await loginPage.type("#telTxtLogin", username);
+  await loginPage.type("#passShow", password);
+  console.log("==Click Button ==");
+  loginPage.on("dialog", async dialog => {
+    if (dialog.message() === "Login success") {
+      console.log("== Login Success ==");
+      promiseArray = [];
+      links.forEach(link => {
+        console.log(`== Elevate thread link: == ${link}`);
+        const task = browser
+          .newPage()
+          .then(async page => {
+            await page.goto(`http://${link}`);
+            await page.click('a[title="提升帖子"]');
+            console.log(`Page ${link} elevated.`)
+          }).catch(e =>
+              console.error(e)
+          )
+        promiseArray.push(task);
+      });
+      console.log("All task are distributed")
+      await Promise.all(promiseArray);
+      browser.close();
+    }
   });
-  await loginPage.click('#postBtn');
+  await loginPage.click("#postBtn");
 })();
